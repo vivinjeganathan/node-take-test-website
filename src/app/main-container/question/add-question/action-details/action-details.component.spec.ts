13a845857161dcca243e0e133fac802224c024a8
Vivin Jeganathan <Vivin.Jeganathan@csgi.com>
2018-07-15 11:11:10 -0500

    misc

--- src/app/main-container/question/add-question/action-details/action-details.component.spec.ts
+++ src/app/main-container/question/add-question/action-details/action-details.component.spec.ts
@@ -23,32 +23,3 @@ describe('ActionDetailsComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-
-
-// export class QuestionsComponent implements OnInit{
-
-//   questionCreated = false
-//   dynamicValue = 'Test';
-//   allowNewServer = false;
-//   questionDescription = 'test'
-//   questionsArray = ['sample1', 'sample2']
-
-//   constructor() {
-//       setTimeout(()=> {
-//           this.allowNewServer = true;
-//       }, 2000);
-//   }
-
-//   ngOnInit() {
-
-//   }
-//   onCreateQuestion() {
-//       this.questionsArray.push(this.questionDescription)
-//       this.questionCreated = true
-//       this.dynamicValue = 'question created with description ' + this.questionDescription;
-//   }
-
-//   onEditDescription(event: any) {
-//       this.questionDescription = (<HTMLInputElement>event.target).value
-//   }
-// }
--- src/app/main-container/question/add-question/action-details/action-details.component.ts
+++ src/app/main-container/question/add-question/action-details/action-details.component.ts
@@ -13,3 +13,31 @@ export class ActionDetailsComponent implements OnInit {
   }
 
 }
+
+// export class QuestionsComponent implements OnInit{
+
+//   questionCreated = false
+//   dynamicValue = 'Test';
+//   allowNewServer = false;
+//   questionDescription = 'test'
+//   questionsArray = ['sample1', 'sample2']
+
+//   constructor() {
+//       setTimeout(()=> {
+//           this.allowNewServer = true;
+//       }, 2000);
+//   }
+
+//   ngOnInit() {
+
+//   }
+//   onCreateQuestion() {
+//       this.questionsArray.push(this.questionDescription)
+//       this.questionCreated = true
+//       this.dynamicValue = 'question created with description ' + this.questionDescription;
+//   }
+
+//   onEditDescription(event: any) {
+//       this.questionDescription = (<HTMLInputElement>event.target).value
+//   }
+// }
